56f6d8ac23e08a0d4b3dba1b1ed4f4348ab12c61
yte0 <Yohannes.Tesfay@helse-nord.no>
2019-10-15 10:36:12 +0200

    pasient og forløpstabell

---
+++ R/MuskelTabellerForlopPas.R
@@ -0,0 +1,38 @@
+#' Forløps of pasient tabeller for Muskelregisteret
+#'
+#' Denne funksjonen tar inn registerdata lager tabeller
+#' av antall pasienter/forløp per sykehus per tidsenhet
+#'
+#' @param RegDt en dataramme
+#' @param tidfra
+#'
+#'
+#' @return tabelldata med antall pasienter/forløp per sykhus
+#' per tidsenhet
+#'
+#' @export
+#'
+
+MuskelTabellerForlopspas <- function(RegDt = RegData, tidFra = "2008-01-01", tidTil = Sys.Date() ,
+                       aldmin = 0, aldmax = 120, kjoen = 99, tidenh = "aar",
+                       frlType = NULL, avd = "Nei", IDType = "PasientID"){
+
+    if (kjoen == 99) { kjoen <- c(0,1)}
+    if(is.null(frlType) ) {frlType <-  unique(RegData$ForlopsType1)}
+    if(avd == "Ja"){avd <-  c("Ja","Nei")}
+
+
+
+    utData <- RegData %>% dplyr::select( PasientID, ForlopsID, SykehusNavn, HovedDato,
+                                         erMann, PasientAlder, Avdod, ForlopsType1Num, ForlopsType1) %>%
+        dplyr::mutate(maaned = factor(lubridate::month(HovedDato),labels = c("Jan","Feb",
+                                                                             "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Des")) ,
+                      aar = lubridate::year(HovedDato) ) %>%
+        dplyr::filter( as.Date(HovedDato) %>% dplyr::between(as.Date(tidFra) ,as.Date(tidTil)),
+                       PasientAlder %>% dplyr::between( aldmin,aldmax), erMann %in% kjoen,
+                       ForlopsType1 %in% frlType, Avdod %in% avd) %>% dplyr::select(PasientID, ForlopsID, SykehusNavn, maaned, aar) %>%
+        dplyr::arrange(aar,maaned)
+    utData <- utData%>% dplyr::filter(!duplicated( utData[[IDType]]))
+
+    tabData <-  stats::addmargins( table(utData[["SykehusNavn"]],utData[[tidenh]] ) )
+}
---
+++ inst/shinyApps/muskel/www/tabell.R
@@ -0,0 +1,86 @@
+headerFooter <- function(data){
+    data <- as.data.frame.matrix(data)
+    dataStr <- dim(data)
+    hr <-  c("", names(data))
+    fr <-  c("sum", data[dataStr[1],1:dataStr[2]])
+    sketch <- htmltools::tags$table(
+        tableHeader(names = hr) ,
+        tableFooter(names = fr ) )
+    return(sketch)
+}
+
+forlop <- unique(RegData$ForlopsType1)
+
+tabellUI <- function(id, datoStart = "2008-01-01",
+                     datoSlutt = Sys.Date(),forltype = forlop){
+    ns <- shiny::NS(id)
+    shiny::sidebarLayout(
+        shiny::sidebarPanel(
+            shiny::dateRangeInput(ns("dato"), "Tidsperiode:",
+                                  start = datoStart, end = datoSlutt,
+                                  format = "yyyy-mm-dd" ),
+
+            shiny::sliderInput(ns("ald"), label = "Alder",
+                               min = 0, max = 120, value = c(0,120) ),
+
+            shiny::selectInput(ns("kjo"), "Kjønn",
+                               choices = c("Begge" = 99, "Kvinne" = 0,
+                                           "Mann" = 1),selected = 99),
+
+            shiny::selectInput(ns("tidenh"), "Velg tidsenhet",
+                               choices = c("Måned" = "maaned", "År" = "aar")),
+
+            shiny::selectInput(ns("forl"), label = "Forløpstype",
+                               choices = forltype, multiple = TRUE),
+
+
+            shiny::selectInput(ns("avdod"), label = "Inkluder avdøde",
+                               choices = c("Ja" , "Nei" ),
+                               selected = "Nei"),
+
+            shiny::radioButtons(ns("skjemarad"), "",
+                                choices = c("Forløp"= "ForlopsID","Pasient"= "PasientID"),
+                                inline = TRUE  )
+        ),
+        shiny::mainPanel(
+            shiny::h3(textOutput(ns("txt1"))),
+            shiny::h5(textOutput(ns("txt2"))),
+            hr(),
+            DT::DTOutput(ns("Tabell")),
+            h6(textOutput(ns("txt"))),
+            shiny::downloadButton(ns("lastNedTabell"), "Last ned tabell")
+        )
+    )
+}
+
+
+
+tabell <- function(input, output, session){
+
+    forloptxt <- reactive({if(input$skjemarad=="ForlopsID") {
+        "registrerte pasientforløp"
+    }else{ "unike pasienter"}  })
+
+    tidenhtxt <- reactive({if(input$tidenh == "maaned") {
+        "måned"
+    }else{ "år"}  })
+
+
+    output$txt1 <- renderText({ paste0("Antall " ,forloptxt()," per ",tidenhtxt(), " per avdeling") })
+
+
+    tabellData <- reactive({ as.data.frame.matrix( muskel::MuskelTabellerForlopspas(RegData, tidFra = input$dato[1],tidTil = input$dato[2],
+                                                              aldmin = input$ald[1], aldmax = input$ald[2], kjoen = input$kjo,
+                                                              tidenh = input$tidenh, avd = input$avdod,IDType = input$skjemarad, frlType = input$forl )
+    ) })
+
+    observe({
+        cont <- headerFooter(tabellData())
+        subS <- dim(tabellData())[1]-1
+
+        output$Tabell <-  renderDT(
+            as.data.frame.matrix(tabellData())[1:subS, ] , container = cont, extensions = 'FixedHeader',
+            options = list(pageLength = 50,fixedHeader = TRUE, lengthChange = FALSE) )
+    })
+
+}
