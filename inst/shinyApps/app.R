8a9839e6e9d423402654c3155570a56119314ab0
yte0 <Yohannes.Tesfay@helse-nord.no>
2019-11-05 13:55:42 +0100

    debug

--- inst/shinyApps/app.R
+++ inst/shinyApps/app.R
@@ -110,13 +110,13 @@ library(shiny)
 
 
 
-system.file("shinyApps/dataOGvar.R",package = "muskel") %>%
+system.file("./shinyApps/dataOGvar.R",package = "muskel") %>%
   source(encoding = "UTF-8")
-system.file("shinyApps/forAndGr.R",package = "muskel") %>%
+system.file("./shinyApps/forAndGr.R",package = "muskel") %>%
   source(encoding = "UTF-8")
-system.file("shinyApps/kumandel.R",package = "muskel") %>%
+system.file("./shinyApps/kumandel.R",package = "muskel") %>%
   source(encoding = "UTF-8")
-system.file("shinyApps/tabell.R",package = "muskel") %>%
+system.file("./shinyApps/tabell.R",package = "muskel") %>%
   source(encoding = "UTF-8")
 
 # Define UI for application that draws a histogram
