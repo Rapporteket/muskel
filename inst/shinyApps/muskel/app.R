7e8bd7e732df34d0bb965b77d17fcf559720c36a
yte0 <Yohannes.Tesfay@helse-nord.no>
2019-11-05 16:36:04 +0100

    debug

--- inst/shinyApps/muskel/app.R
+++ inst/shinyApps/muskel/app.R
@@ -169,8 +169,7 @@ ui <- navbarPage(#title = "RAPPORTEKET MUSKELREGISTERET", theme = "bootstrap.css
                           mainPanel(tabsetPanel(
                             tabPanel("Figur",
                                      plotOutput("Figur1", height="auto"),
-                                     h3(paste0("xsdkjhfkadhs",system.file(package = "muskel"),
-                                               list.files(R.home())     )),
+                                     h3(paste0("xsdkjhfkadhs",system.file(package = "muskel")     )),
                                      downloadButton("lastNedBilde", "Last ned bilde")),
                             tabPanel("Tabell",
                                      tableOutput("Tabell1"),
