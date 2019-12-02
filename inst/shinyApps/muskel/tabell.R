91bab04dd771c52171e9d56eab846eed20e582e7
yte0 <Yohannes.Tesfay@helse-nord.no>
2019-12-02 10:54:00 +0100

    nedlasting av data, 3 mnd, 6mnd og 1-år knapper med eventHandlers

--- inst/shinyApps/muskel/tabell.R
+++ inst/shinyApps/muskel/tabell.R
@@ -11,43 +11,51 @@ headerFooter <- function(data){
 
 forlop <- unique(RegData$ForlopsType1)
 
-tabellUI <- function(id, datoStart = "2008-01-01",
-                     datoSlutt = Sys.Date(),forltype = forlop){
+tabellUI <- function(
+    id, datoStart = "2008-01-01",
+    datoSlutt = Sys.Date(),forltype = forlop){
     ns <- shiny::NS(id)
     shiny::sidebarLayout(
         shiny::sidebarPanel(
-            shiny::dateRangeInput(ns("dato"), "Tidsperiode:",
-                                  start = datoStart, end = datoSlutt,
-                                  format = "yyyy-mm-dd" ),
-
-            shiny::sliderInput(ns("ald"), label = "Alder",
-                               min = 0, max = 120, value = c(0,120) ),
-
-            shiny::selectInput(ns("kjo"), "Kjønn",
-                               choices = c("Begge" = 99, "Kvinne" = 0,
-                                           "Mann" = 1),selected = 99),
-
-            shiny::selectInput(ns("tidenh"), "Velg tidsenhet",
-                               choices = c("Måned" = "maaned", "År" = "aar")),
-
-            shiny::selectInput(ns("forl"), label = "Forløpstype",
-                               choices = forltype, multiple = TRUE),
-
-
-            shiny::selectInput(ns("avdod"), label = "Inkluder avdøde",
-                               choices = c("Ja" , "Nei" ),
-                               selected = "Nei"),
-
-            shiny::radioButtons(ns("skjemarad"), "",
-                                choices = c("Forløp"= "ForlopsID","Pasient"= "PasientID"),
-                                inline = TRUE  )
+            shiny::dateRangeInput(
+                ns("dato"), "Tidsperiode:",
+                start = datoStart, end = datoSlutt,
+                format = "yyyy-mm-dd"
+            ),
+            shiny::sliderInput(
+                ns("ald"), label = "Alder",
+                min = 0, max = 120, value = c(0,120)
+            ),
+            shiny::selectInput(
+                ns("kjo"), "Kjønn",
+                choices = c(
+                    "Begge" = 99, "Kvinne" = 0, "Mann" = 1),
+                selected = 99
+            ),
+            shiny::selectInput(
+                ns("tidenh"), "Velg tidsenhet",
+                choices = c("Måned" = "maaned", "År" = "aar")
+            ),
+            shiny::selectInput(
+                ns("forl"), label = "Forløpstype",
+                choices = forltype, multiple = TRUE
+            ),
+            shiny::selectInput(
+                ns("avdod"), label = "Inkluder avdøde",
+                choices = c("Ja" , "Nei" ), selected = "Nei"
+            ),
+            shiny::radioButtons(
+                ns("skjemarad"), "",
+                choices = c("Forløp"= "ForlopsID","Pasient"= "PasientID"),
+                inline = TRUE
+            )
         ),
         shiny::mainPanel(
-            shiny::h3(textOutput(ns("txt1"))),
-            shiny::h5(textOutput(ns("txt2"))),
-            hr(),
+            shiny::h3(textOutput(ns("txt1")),
+                      style = "text-align:center"),
+            shiny::uiOutput(ns("tidsIntervall")),
+            #hr(),
             DT::DTOutput(ns("Tabell")),
-            h6(textOutput(ns("txt"))),
             shiny::downloadButton(ns("lastNedTabell"), "Last ned tabell")
         )
     )
@@ -68,27 +76,112 @@ tabell <- function(input, output, session){
 
     output$txt1 <- renderText({ paste0("Antall " ,forloptxt()," per ",tidenhtxt(), " per avdeling") })
 
+    output$tidsIntervall <- renderUI({
+        ns <- session$ns
+        ic <- icon("calendar-alt")
+        st <- "color : grey ; background-color:white "
+        if ( req(input$tidenh) == "maaned") {
+            tagList(
+                shiny::fluidRow(
+                    column(3,offset = 9,
+                        shiny::actionButton(ns("tre"), "3 mnd",
+                                            ic, style = st, width = "30%"),
+                        shiny::actionButton(ns("seks"), "6 mnd",
+                                            ic, style = st,width = "30%"),
+                        shiny::actionButton(ns("et"), "1 år", ic,
+                                            style =st,width = "30%")
+                    )
+                )
+            )
+        }
+    })
 
+    #
+    observeEvent(input$tre,{
+        valgtDato <- as.Date(max(input$dato)) -
+            lubridate::day(as.Date(max(input$dato))) + 1
+
+        shiny::updateDateRangeInput(
+            session,
+            inputId = "dato",
+            start = valgtDato %m-% months(3),
+        )
+    })
+    observeEvent(input$seks,{
+        valgtDato <- as.Date(max(input$dato)) -
+            lubridate::day(as.Date(max(input$dato))) + 1
+
+        shiny::updateDateRangeInput(
+            session,
+            inputId = "dato",
+            start = valgtDato %m-% months(6),
+        )
+    })
+    observeEvent(input$et,{
+        valgtDato <- as.Date(max(input$dato)) -
+            lubridate::day(as.Date(max(input$dato))) + 1
+
+        shiny::updateDateRangeInput(
+            session,
+            inputId = "dato",
+            start = valgtDato %m-% months(12),
+        )
+    })
+
+
+
+    ##table data
     tabellData <- reactive({ as.data.frame.matrix(
-        muskel::MuskelTabellerForlopspas(RegData,
-                                         tidFra = req(input$dato[1]),
-                                         tidTil = req(input$dato[2]),
-                                         aldmin = req(input$ald[1]),
-                                         aldmax = req(input$ald[2]),
-                                         kjoen = req(input$kjo),
-                                         tidenh = req(input$tidenh),
-                                         avd = req(input$avdod),
-                                         IDType = req(input$skjemarad),
-                                         frlType = (input$forl) )
-    ) })
+        muskel::MuskelTabellerForlopspas(
+            RegData,
+            tidFra = req(input$dato[1]),
+            tidTil = req(input$dato[2]),
+            aldmin = req(input$ald[1]),
+            aldmax = req(input$ald[2]),
+            kjoen = req(input$kjo),
+            tidenh = req(input$tidenh),
+            avd = req(input$avdod),
+            IDType = req(input$skjemarad),
+            frlType = (input$forl))
+        )
+    })
 
+    #render table
     observe({
         cont <- headerFooter(tabellData())
         subS <- dim(tabellData())[1]-1
 
         output$Tabell <-  renderDT(
-            as.data.frame.matrix(tabellData())[1:subS, ] , container = cont, extensions = 'FixedHeader',
-            options = list(pageLength = 50,fixedHeader = TRUE, lengthChange = FALSE) )
+            as.data.frame.matrix(tabellData())[1:subS, ] %>%
+                DT::datatable(
+                    container = cont,
+                    extensions = 'FixedHeader',
+                    selection = "none",
+                    options = list(
+                        pageLength = 50,
+                        fixedHeader = TRUE,
+                        lengthChange = FALSE,
+                        dom = "t"))
+        )
     })
+
+    output$lastNedTabell <- downloadHandler(
+        filename = function() {
+            if (req(input$skjemarad) == "PasientID") {
+                paste0("pasienttabell",
+                       req(input$tidenh),
+                       Sys.time(), ".csv")
+            } else {
+                paste0("forlopstabell",
+                       req(input$tidenh),
+                       Sys.time(), ".csv")
+            }
+        },
+        content = function(file) {
+            tab <- tabellData()
+            write.csv2(tab, file, row.names = T)
+        }
+    )
+
 
 }
