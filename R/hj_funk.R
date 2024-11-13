ad9c8863dbf5875d87f400ee3a1af9212e4aa544
kevinthon <kevin.thon@gmail.com>
2024-11-13 13:55:31 +0100

    påbegynt smarapport

--- R/hj_funk.R
+++ R/hj_funk.R
@@ -126,3 +126,28 @@ MuskelTabellerForlopspas <- function(
   }
 }
 
+#' Transponer output fra dplyr::summarize
+#'
+#' Denne funksjonen tar som input resultatet av dplyr::summarize og returnerer dens
+#' transponerte uten at formatene endres.
+#'
+#' Her kan detaljer skrives
+#'
+#' @return tr_frame Den transponerte av inputen
+#'
+#' @export
+#'
+tr_summarize_output <- function(x, kolnavn1 = ""){
+
+  rekkefolge <- names(x)[-1]
+  y <- x %>% gather(names(x)[-1], key=nokkel, value = verdi) %>%
+    spread(key=names(x)[1], value = verdi)
+  y <- y[match(rekkefolge, y$nokkel), ]
+  names(y)[1] <- kolnavn1
+
+  return(invisible(y))
+}
+
+
+
+
